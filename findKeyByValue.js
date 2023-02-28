7c72b12b15a8f42aa44ad3f3d33e81644e2d62c8
Nicole Law <nicole.law12@gmail.com>
2023-02-28 15:29:29 -0500

    finishing findKeyByValues

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,5 +1,4 @@
 
-/*
 // function that makes sure the the expected value(s) === to actual value of the function you are running
 const assertEqual = function(actual, expected) {
   if (actual === expected) {// if actual equals to expected
@@ -8,30 +7,32 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);// Fails
   }
 };
-*/ 
 
 //function that scans the object and return the first key which contains the given value. If no key with that value, return undefined.
-const findKeyByValue = function (object, value) {
-  const key = "";// creating an empty string for our final key  
-  const keys = Object.keys(object);// creating a var keys, that will display all keys in the object 
-    for (let i = 0; i < keys.length; i++) {// looping through keys  
-      if (object[keys[i] === value]) {
-        console.log(keys);
-      } else {
-        return undefined 
-      }   
+const findKeyByValue = function(object, value) {
+  let keyFinal = "";// creating an empty string for our final key
+  const keys = Object.keys(object);// creating a var keys, that will display all keys in the object
+  //console.log(keys);// double check it displays all keys
+  for (let i = 0; i < keys.length; i++) {// looping through keys
+    //console.log(object[keys[i]]);// double check it displays all values
+    const multipleValues = object[keys[i]];// create var multipleValues, that will display all values in the object
+    if (multipleValues === value) {// if values equals to inputted value
+      keyFinal = keys[i];// key is assigned to keyFinal
+    } else {
+      keyFinal = undefined;// if values do not equal to inputted value, undefined is assigned
+    }
   }
-} ; 
+  return keyFinal;
+};
 
-
-const bestTVShowsByGenre = { 
+const bestTVShowsByGenre = {
   sci_fi: "The Expanse",
   comedy: "Brooklyn Nine-Nine",
   drama:  "The Wire"
 };
 
 findKeyByValue(bestTVShowsByGenre, "The Wire");
+findKeyByValue(bestTVShowsByGenre, "That '70s Show");
 
-
-//assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-//assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
