8b9f6a4896d6b814522086a3fc4ed2151f222a4b
Nicole Law <nicole.law12@gmail.com>
2023-02-27 19:26:58 -0500

    updated letterPositions

--- letterPositions.js
+++ letterPositions.js
@@ -1,23 +1,3 @@
-const letterPositions = function(sentence) {
-  const results = {};
-  for (let i = 0; i < sentence.length; i++) { // go through each letter of the sentence
-    if (sentence[i] !== " ") { // letter doesn't equal to space is true!
-      if (!results[sentence[i]]) { // {i} does not exist then...
-        results[sentence[i]] = [i]; 
-      } else { // if {i} already exists 
-        results[sentence[i]].push(i);
-      }
-    }
-  }
-  console.log(results);
-};
-
-letterPositions("lighthouse in the house");
-letterPositions("hello");
-
-
-assertArraysEqual(letterPositions("hello").e, [1]);
-
 const eqArrays = (arr1, arr2) => {
   if (arr1.length !== arr2.length) {
     return false;
@@ -32,27 +12,30 @@ const eqArrays = (arr1, arr2) => {
 };
 
 const assertArraysEqual = function(arr1, arr2) {
-  let sameLength = arr1.length === arr2.length;
-  let sameValues = true;
-  for (let i = 0; i <= arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      sameValues = false;
-      break;
-    }
-  }
-  if (sameValues && sameLength) {
+  if (eqArrays(arr1, arr2)) {
     console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
   } else {
     console.log(`🛑🛑🛑Assertion Failed: ${arr1} !== ${arr2}`);
   }
 };
 
-/*
-const assertArraysEqual = function(arr1, arr2) {
-   if (eqArrays(arr1, arr2)) {
-    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🛑🛑🛑Assertion Failed: ${arr1} !== ${arr2}`);
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) { // go through each letter of the sentence
+    if (sentence[i] !== " ") { // letter doesn't equal to space is true!
+      if (!results[sentence[i]]) { // {i} does not exist then... / checking if the letter in sentence exists in the object already...
+        results[sentence[i]] = [i]; // if not assign it the array, index
+      } else { // if {i} already exists
+        results[sentence[i]].push(i); // arrays can have more than one, pushing
+      }
+    }
   }
+  return results;
 };
-*/ 
+
+letterPositions("lighthouse in the house");
+letterPositions("hello");
+
+assertArraysEqual(letterPositions("hello").e, [1]);
+assertArraysEqual(letterPositions("hello").l, [2, 3]);
+
