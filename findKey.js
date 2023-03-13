c4bf42d821326d17d775753848cad46d4bd52087
Nicole Law <nicole.law12@gmail.com>
2023-03-13 14:42:33 -0400

    adding Mocha & Chai

--- findKey.js
+++ findKey.js
@@ -9,7 +9,7 @@ const assertEqual = function(actual, expected) {
 
 const findKey = function(object, callback) {
   for (let key in object) {// looping through object keys
-    if (callback(object[key])) {// if callback function is truthy 
+    if (callback(object[key])) {// if callback function is truthy
       return key;
     }
   }
@@ -30,7 +30,7 @@ assertEqual(findKey({  "Blue Hill": { stars: 1 },
 "elBulli":   { stars: 3 },
 "Ora":       { stars: 2 },
 "Akelarre":  { stars: 3 }
-}, x => x.stars === 2), "noma"); 
+}, x => x.stars === 2), "noma");
 
 assertEqual(findKey({  "Blue Hill": { stars: 1 },
 "Akaleri":   { stars: 3 },
@@ -38,7 +38,7 @@ assertEqual(findKey({  "Blue Hill": { stars: 1 },
 "elBulli":   { stars: 3 },
 "Ora":       { stars: 2 },
 "Akelarre":  { stars: 3 }
-}, x => x.stars === 3), "Akaleri"); 
+}, x => x.stars === 3), "Akaleri");
 
 assertEqual(findKey({  "Blue Hill": { stars: 1 },
 "Akaleri":   { stars: 3 },
@@ -46,4 +46,4 @@ assertEqual(findKey({  "Blue Hill": { stars: 1 },
 "elBulli":   { stars: 3 },
 "Ora":       { stars: 2 },
 "Akelarre":  { stars: 3 }
-}, x => x.stars === 4), "Akaleri"); // fail 
+}, x => x.stars === 4), "Akaleri"); // fail
